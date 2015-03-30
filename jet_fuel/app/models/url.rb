2f86e8ec2a5e4bb14c564b91cf0ffff6601d8a60
Laura Whalin <laurawhalin@gmail.com>
2015-03-29 17:02:25 -0700

    changed single to double quotes for hound

--- jet_fuel/app/models/url.rb
+++ jet_fuel/app/models/url.rb
@@ -3,7 +3,7 @@ class Url < ActiveRecord::Base
   validates :short_url, presence: true, uniqueness: true
 
   def self.generate
-    randomstring = ((("a".."z").to_a + (0..9).to_a) * 3).shuffle[ 0, 5.to_i ].join
+    randomstring = ((("a".."z").to_a + (0..9).to_a) * 3).shuffle[0, 5.to_i].join
     "#{randomstring}"
   end
 
