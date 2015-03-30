809ff1d0c15f29fa8711963c1d574171b4971b9a
Laura Whalin <laurawhalin@gmail.com>
2015-03-29 17:11:43 -0700

    hound

--- jet_fuel/db/migrate/20150325171532_set_default_tally_for_urls.rb
+++ jet_fuel/db/migrate/20150325171532_set_default_tally_for_urls.rb
@@ -1,5 +1,5 @@
 class SetDefaultTallyForUrls < ActiveRecord::Migration
   def change
-    change_column :urls, :click_tally, :integer, :default => 0
+    change_column :urls, :click_tally, :integer, default: 0
   end
 end
